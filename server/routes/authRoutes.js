f2e916cdfcd5ead82a8981820ca4a61cddf3236a
Subhadip Mondal <subhadipmondal0101@gmail.com>
2026-02-21 21:15:51 +0530

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,1 +1,9 @@
 
+import { Router } from 'express';
+import { resolveEmailByUsername } from '../controllers/authController.js';
+
+const router = Router();
+
+router.get('/email-by-username', resolveEmailByUsername);
+
+export default router;
